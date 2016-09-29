dc1de42e88abc104d7b1be15c43139ba6bdce1bb
Zimovets-Viktoriya696 <zc.vika@gmail.com>
2016-09-29 14:47:14 +0300

    conect

--- src/main/java/com/conct/Main.java
+++ src/main/java/com/conct/Main.java
@@ -2,12 +2,14 @@ package com.conct;
 
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
-import java.beans.Statement;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
+import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  * Created by Виктория on 26.09.16.
  */
@@ -17,24 +19,40 @@ public class Main {
     private final static String USERNAME = "root";
     private final static String PASSWORD = "1234";
 
-
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
 
     public static void main(String[] args) {
+        String query = "select Sample_TDate_1  from akhz1_data_50";
 
         try {
-            Driver driver = new FabricMySQLDriver();
-            DriverManager.registerDriver(driver);}
-        catch (Exception e) {
-            System.out.println("Не удалось загрузить драйвер");}
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
-        try {
-            Connection connection  = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            java.sql.Statement statement =  connection.createStatement();
-            statement.getConnection();
-        }
-         catch (SQLException e) {
-            e.printStackTrace();
+            // getting Statement object to execute query
+            stmt = con.createStatement();
+
+            // executing SELECT query
+            rs = stmt.executeQuery(query);
+
+
+            while (rs.next()) {
+                String id = rs.getString("Sample_TDate_1");
+              //  int name = rs.getInt("Sample_MSec_1");
+              //  double author = rs.getDouble("Sample_Value_1");
+                System.out.println(id);
+            }
+
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            //close connection ,stmt and resultset here
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
         }
     }
-    }
+
+}
 
