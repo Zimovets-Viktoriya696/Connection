fb173db9ff003176dceefe952cc2e3bc1cbdb27b
Zimovets-Viktoriya696 <zc.vika@gmail.com>
2016-10-14 22:26:38 +0300

    qwery

--- src/main/java/com/conct/Main.java
+++ src/main/java/com/conct/Main.java
@@ -19,39 +19,13 @@ public class Main {
     private final static String USERNAME = "root";
     private final static String PASSWORD = "1234";
 
-    private static Connection con;
-    private static Statement stmt;
-    private static ResultSet rs;
+
 
     public static void main(String[] args) {
-        String query = "select Sample_TDate_1  from akhz1_data_50";
 
-        try {
-            // opening database connection to MySQL server
-            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
-            // getting Statement object to execute query
-            stmt = con.createStatement();
-
-            // executing SELECT query
-            rs = stmt.executeQuery(query);
-
-
-            while (rs.next()) {
-                String id = rs.getString("Sample_TDate_1");
-              //  int name = rs.getInt("Sample_MSec_1");
-              //  double author = rs.getDouble("Sample_Value_1");
-                System.out.println(id);
-            }
-
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        } finally {
-            //close connection ,stmt and resultset here
-            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
-            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
-            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
-        }
+        MyDatabaseDriver myDatabaseDriver = new MyDatabaseDriver(URL, USERNAME, PASSWORD);
+        myDatabaseDriver.getData();
+        myDatabaseDriver.close();
     }
 
 }
