753ad3ee4266563345cb4e2c62e0e85a95e2e0d6
Zimovets-Viktoriya696 <zc.vika@gmail.com>
2016-10-14 21:51:52 +0300

    qwery

--- src/main/java/com/conct/MyDatabaseDriver.java
+++ src/main/java/com/conct/MyDatabaseDriver.java
@@ -1,17 +1,15 @@
 package com.conct;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by Виктория on 29.09.16.
  */
 public class MyDatabaseDriver {
 
-    private static Connection con;
-    private static Statement stmt;
+    private  Connection con;
+    private  Statement stmt;
+    private  ResultSet rs;
 
 
     public MyDatabaseDriver(String url, String username, String password){
@@ -20,12 +18,26 @@ public class MyDatabaseDriver {
             con = DriverManager.getConnection(url, username, password);
             // getting Statement object to execute query
             stmt = con.createStatement();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
 
-public
-
+    public void getMethod (){
+        String query = "select Sample_TDate_1  from akhz1_data_50";
+            try {
+            rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                String id = rs.getString("Sample_TDate_1");
+                //  int name = rs.getInt("Sample_MSec_1");
+                //  double author = rs.getDouble("Sample_Value_1");
+                System.out.println(id);
+                    }
+                }
+            catch (SQLException sqlEx){
+        sqlEx.printStackTrace();
+        }
+    }
 }
