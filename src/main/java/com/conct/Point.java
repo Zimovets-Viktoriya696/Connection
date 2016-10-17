605b9547ef7b624eb94c3ea5695d8dd8bb210c52
Zimovets-Viktoriya696 <zc.vika@gmail.com>
2016-10-17 21:48:09 +0300

    points

---
+++ src/main/java/com/conct/Point.java
@@ -0,0 +1,34 @@
+package com.conct;
+
+import java.util.Comparator;
+
+/**
+ * Created by Виктория on 17.10.16.
+ */
+public class Point {
+    private long _time;
+    public long getTime() { return _time; }
+    private float _value;
+    public float getValue() { return _value; }
+
+    public Point(long time, float value)
+    {
+        _time = time;
+        _value = value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("{time=%d, value=%f}", _time, _value);
+    }
+}
+
+class TimeComparator implements Comparator<Point>
+{
+    @Override
+    public int compare(Point a, Point b)
+    {
+        return a.getTime() < b.getTime() ? -1 : a.getTime() == b.getTime() ? 0 : 1;
+    }
+}
